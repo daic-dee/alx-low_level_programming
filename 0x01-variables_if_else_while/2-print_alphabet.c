c82f969bee95ed6fa81405a138a561f8618ec46f
dee-daic <daickate@gmail.com>
2023-03-17 03:52:11 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -A program to print alphabet
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter < = 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('n');
+	return (0);
+}
