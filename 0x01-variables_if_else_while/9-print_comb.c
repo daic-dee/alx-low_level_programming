2c8b1cced9053bafb813179a8b7dece49e1e5cfe
dee-daic <daickate@gmail.com>
2023-03-17 05:07:48 -0700

    single-digit combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - print all combinations
+ * Return: 0
+ */
+
+int main(void)
+{
+	putchar('0');
+	putchar(',');
+	putchar(' ');
+	putchar('1');
+	putchar(',');
+	putchar(' ');
+	putchar('2');
+	putchar(',');
+	putchar(' ');
+	putchar('3');
+	putchar(',');
+	putchar(' ');
+	putchar('4');
+	putchar(',');
+	putchar(' ');
+	putchar('5');
+	putchar(',');
+	putchar(' ');
+	putchar('6');
+	putchar(',');
+	putchar(' ');
+	putchar('7');
+	putchar(',');
+	putchar(' ');
+	putchar('8');
+	putchar(',');
+	putchar(' ');
+	putchar('9');
+	putchar('\n');
+	return (0);
+}
