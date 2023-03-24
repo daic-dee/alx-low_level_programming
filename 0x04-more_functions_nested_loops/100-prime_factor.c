02e6634cf9b171a146785acaae2b812713c35c99
dee-daic <daickate@gmail.com>
2023-03-24 09:11:01 -0700

    print number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,13 +5,13 @@
  * Return: 0
  */
 int main(void)
-{       
+{
 	unsigned long int i, n = 612852475143;
 
 	for (i = 3; i < 782849; i = i + 2)
 	{
 		while ((n % i == 0) && (n != i))
-			n = n /i;
+			n = n / i;
 	}
 	printf("%lu\n", n);
 	return (0);
