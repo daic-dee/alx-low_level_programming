a524364954ec0a13056ad53cb466ab69b12bc64e
dee-daic <daickate@gmail.com>
2023-03-27 16:57:39 -0700

    calculate string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns length of a string
+ * @s: string
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
