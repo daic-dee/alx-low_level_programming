037d139e2913e35b5a51fadf5e72937051c5eae1
dee-daic <daickate@gmail.com>
2023-04-14 15:29:04 +0000

    static libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,17 +1,13 @@
 #include "main.h"
 /**
- * _puts - puts char
- * @str: string value
- * Return: Always 0 (Success)
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
  */
-
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0; str[i] != 0; i++)
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
+		_putchar(*str++);
 	}
 	_putchar('\n');
 }
