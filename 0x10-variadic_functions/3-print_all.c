291835b02845d4b2d12c5b9b08fcd791eb02ee3d
dee-daic <daickate@gmail.com>
2023-04-21 02:34:31 +0000

    variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -32,8 +32,10 @@ void print_all(const char * const format, ...)
 						break;
 				case 's':
 						str = va_arg(list, char *);
-						if (!str)
-							str = "(nil)";
+					if (!str)
+					{
+						str = "(nil)";
+					}
 						printf("%s%s", sep, str);
 						break;
 				default:
